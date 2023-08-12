a9b2b12fce4b219a67c6caca7466aa60fcdf457f
Siddhesh Mhapankar <72028899+siddheshmhapankar2478@users.noreply.github.com>
2023-08-12 17:16:45 +0530

    Update Shimmer.js

--- app/components/Shimmer.js
+++ app/components/Shimmer.js
@@ -5,10 +5,10 @@ const Shimmer = () => {
     <table className="w-full border-collapse border border-gray-300 mb-10 animate-pulse">
         <thead>
           <tr className="bg-[rgb(54,69,79)]">
-            <th className="h-4  py-4 px-4 m-1 border"><div className='w-3/5 h-6 mx-auto bg-slate-400'></div></th>
-            <th className="h-4  py-4 px-4 m-1 border"><div className='w-3/5 h-6 mx-auto bg-slate-400'></div></th>
-            <th className="h-4  py-4 px-4 m-1 border"><div className='w-3/5 h-6 mx-auto bg-slate-400'></div></th>
-            <th className="h-4  py-4 px-4 m-1 border"><div className='w-3/5 h-6 mx-auto bg-slate-400'></div></th>
+            <th className="h-4  py-4 px-4 m-1 border"><div className='w-3/5 h-6 mx-auto bg-slate-400 xs:w-full sm:w-full'></div></th>
+            <th className="h-4  py-4 px-4 m-1 border"><div className='w-3/5 h-6 mx-auto bg-slate-400 xs:w-full sm:w-full'></div></th>
+            <th className="h-4  py-4 px-4 m-1 border"><div className='w-3/5 h-6 mx-auto bg-slate-400 xs:w-full sm:w-full'></div></th>
+            <th className="h-4  py-4 px-4 m-1 border"><div className='w-3/5 h-6 mx-auto bg-slate-400 xs:w-full sm:w-full'></div></th>
           </tr>
         </thead>
         <tbody>
@@ -16,10 +16,10 @@ const Shimmer = () => {
         Array(5).fill("").map((e,index)=>{
             return(
             <tr key={index} className="border">
-              <td className="h-4 w-2 py-5  px-4 m-1 border"><div className='w-3/5 h-6 mx-auto bg-slate-400'></div></td>
-              <td className="h-4 w-2 py-5  px-4 m-1 border"><div className='w-3/5 h-6 mx-auto bg-slate-400'></div></td>
-              <td className="h-4 w-2 py-5  px-4 m-1 border"><div className='w-3/5 h-6 mx-auto bg-slate-400'></div></td>
-              <td className="h-4 w-2 py-5  px-4 m-1 border"><div className='w-3/5 h-6 mx-auto bg-slate-400'></div></td>
+              <td className="h-4 w-2 py-5  px-4 m-1 border"><div className='w-3/5 h-6 mx-auto bg-slate-400 xs:w-full sm:w-full'></div></td>
+              <td className="h-4 w-2 py-5  px-4 m-1 border"><div className='w-3/5 h-6 mx-auto bg-slate-400 xs:w-full sm:w-full'></div></td>
+              <td className="h-4 w-2 py-5  px-4 m-1 border"><div className='w-3/5 h-6 mx-auto bg-slate-400 xs:w-full sm:w-full'></div></td>
+              <td className="h-4 w-2 py-5  px-4 m-1 border"><div className='w-3/5 h-6 mx-auto bg-slate-400 xs:w-full sm:w-full'></div></td>
             </tr>)
         })
         }
