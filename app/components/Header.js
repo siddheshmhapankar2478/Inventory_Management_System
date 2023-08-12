922cdd78ab2152847c14bd8c40f71b90443bfc77
Siddhesh Mhapankar <72028899+siddheshmhapankar2478@users.noreply.github.com>
2023-08-12 22:01:54 +0530

    Update Header.js

--- app/components/Header.js
+++ app/components/Header.js
@@ -9,7 +9,7 @@ const Header = () => {
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
           <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
         </svg>
-        <span className="ml-3 text-xl font-extrabold text-white sm:text-center xs:text-center">Invy</span>
+        <a href="/"><span className="ml-3 text-xl font-extrabold text-white sm:text-center xs:text-center">AtomStock</span></a>
       </a>
       {/* <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
         <a className="mr-5 hover:text-gray-900">First Link</a>
