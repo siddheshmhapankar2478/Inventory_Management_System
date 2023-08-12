7e49ffb608468a54d8713dece1fa3f3fe13ffefc
Siddhesh Mhapankar <72028899+siddheshmhapankar2478@users.noreply.github.com>
2023-08-12 17:16:02 +0530

    Update Footer.js

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -7,8 +7,8 @@ const Footer = () => {
   return (
     <div className="flex flex-wrap bg-[#36454F] text-white justify-center mt-auto">
         <div className="flex xs:flex-col">
-          <h1 className="py-4 ">This website is created by  &nbsp;</h1> 
-          <h1 className='font-bold xs:block xs:text-center py-4'> {name}</h1>
+          <h1 className="py-4 xs:py-2">This website is created by  &nbsp;</h1> 
+          <h1 className='font-bold xs:block xs:text-center py-4 xs:py-2'> {name}</h1>
         </div>    
       <div className="flex flex-wrap mt-auto justify-center">
           <a className="p-2 border rounded-full m-2 hover:text-[#F8781D] hover:border-[#F8781D]" target='_blank' href={`mailto:${mail}`}><MailOutlineIcon></MailOutlineIcon></a>
